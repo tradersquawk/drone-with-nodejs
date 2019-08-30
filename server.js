e6c2abcd7999e1aa2aed622b69087d3f695070cf
Archie Gilbert <archiegilbert@me.com>
2019-08-30 14:46:12 +0100

    new change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-console.log('Fuck off');
+console.log('Fuck off again');
 app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
